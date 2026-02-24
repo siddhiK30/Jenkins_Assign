ccb132904514873d0fad95ca3344e7d151f4630f
siddhiK30 <kumbharsiddhi30@gmail.com>
2026-02-24 16:57:02 +0530

    Updated calculator project with tests

---
+++ calculatorapp/src/test/java/com/demo/calculatorapp/CalculatorTest.java
@@ -0,0 +1,22 @@
+package calculatorapp.src.test.java.com.demo.calculatorapp;
+
+import org.junit.jupiter.api.Test;
+
+import calculatorapp.src.main.java.com.demo.calculatorapp.Calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator cal = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(15, cal.add(10,5));
+    }
+
+    @Test
+    void testSub() {
+        assertEquals(5, cal.sub(10,5));
+    }
+}
